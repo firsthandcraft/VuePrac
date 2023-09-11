c87819aed086ebddeb596a7a00b4534c25681431
firsthandcraft <opaldust19@gmail.com>
2023-09-11 17:22:11 +0900

    ㅇㅇㅇㅇ

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Vue.js 중급 강좌 - 웹앱 제작으로 배워보는 Vue.js, ES6, Vuex
 ## vue.js ver2
+
+#### 섹션 1
+[localStorage에 todo데이터를 넣기](VuePrac
+/my-project)
---
+++ my-project-ver2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <TodoHeader/>
+    <TodoInput/>
+    <TodoList/>
+    <TodoFooter/>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  name: 'App',
+  components: { // '컴포넌트이름 : 컴포넌트 내용'
+    'TodoHeader':TodoHeader,'TodoInput':TodoInput,'TodoList':TodoList,'TodoFooter':TodoFooter
+  }
+}
+</script>
+
+<style>
+  body{
+    text-align:center;background-color:#f6f6f6;
+
+  }
+  input{
+    border-style:groove;
+  }
+  .shadow{
+    box-shadow:5px 10px 10px rgba(0, 0, 0, 0.3);
+  }
+</style>
