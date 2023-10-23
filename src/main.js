2786fca63158b38e79b80fbce1eec03aec8170ba
firsthandcraft <opaldust19@gmail.com>
2023-10-23 23:52:25 +0900

    kakao

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import request from './utils/request'
+import cookie from '@/utils/cookie.js'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+Vue.use(router)
+// Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.prototype.$axios = request
+Vue.prototype.$cookie = cookie
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
+
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', name: 'Main', component: () => import('@/views/Main') },
+    { path: '/kakao-login', name: 'KakaoLogin', component: () => import('@/views/KakaoLogin') },
+    { path: '/naver-login', name: 'NaverLogin', component: () => import('@/views/NaverLogin') },
+    { path: '/google-login', name: 'GoogleLogin', component: () => import('@/views/GoogleLogin') }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+    // eslint-disable-next-line no-unused-vars
+    scrollBehavior(to, from, savedPosition) {
+        return { x: 0, y: 0 }
+    }
+})
+
+export default router
